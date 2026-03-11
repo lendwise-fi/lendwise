77e66f0c6d8adb6aef7dd96cfa70e18216654d3a
Cedric <cedric@smartt.dev>
2026-03-11 10:37:12 +0100

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 
 import { Inter } from 'next/font/google'
 
+import { Analytics } from '@vercel/analytics/next'
 import { Toaster } from 'sonner'
 
 import { AppSidebar } from '@/components/app-sidebar'
